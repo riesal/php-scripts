6d0ad217f18a3ce275928235c8ef611c196be03c
riesal <riesal@gmail.com>
2016-10-03 22:59:04 +0700

    Update array_to_string.php

--- array_to_string.php
+++ array_to_string.php
@@ -8,14 +8,20 @@ echo $bluemix;
 
 if (preg_match("/\bInvalid IP\b/i", $bluemix)) {
     $json = json_decode($bluemix, true);
-    $ipku = $json['message'];
-    $ipku = $ipku . PHP_EOL;
-    $ipku = (string) $ipku;
-    $ipku = end(explode(' ', $ipku));
-    #$ipku = array_pop(explode(' ', $ipku));
-    echo "The IP is invalid, adding this IP: " . $ipku . " to WHMCS\n";
+    $my_ip = $json['message'];
+    $my_ip = $my_ip . PHP_EOL;
+    $my_ip = (string) $my_ip;
+    # $my_ip = end(explode(' ', $my_ip)); OR
+    # $my_ip = array_pop(explode(' ', $my_ip));
+    # applying the variable above will produce: 
+    # Strict Standards: Only variables should be passed by reference
+    # so, the fix is just putting things outside of things, like below:
+    $is_explode = explode(' ', $my_ip);
+    $my_ip = strtolower(array_pop($is_explode));
+    echo "The IP is invalid, adding this IP: " . $my_ip . " to WHMCS\n";
 }
 else
 {
     echo "The IP is valid, nothing todo.\n";
 }
+?>
